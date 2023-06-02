8b9c2679caa585ebde56e96e8ae01a1eeb1fb781
Chandra Mulyana <motorcb100@gmail.com>
2023-06-02 17:03:37 +0700

    20230602 First Commit Next Fonts

---
+++ README.md
@@ -0,0 +1,5 @@
+# Next.js 13 dengan Google Fonts
+
+```
+npm i @next/font
+```
---
+++ app/page.js
@@ -0,0 +1,6 @@
+// ini sama dengan index.js di Page Router
+// A page.js file is required to make a route segment publicly accessible.
+
+export default function Page() {
+	return <h1>Hello, Next.js!</h1>;
+}
